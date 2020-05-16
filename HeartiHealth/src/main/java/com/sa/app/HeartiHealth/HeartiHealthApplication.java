2a5893a91bcd704f01da5ba28a7480faeb55253b
TFarheen <TFarheen@OPT-LAP-0375.Opteamix.COM>
2020-05-16 17:58:31 +0530

    HeartiHealth project

---
+++ HeartiHealth/src/main/java/com/sa/app/HeartiHealth/HeartiHealthApplication.java
@@ -0,0 +1,13 @@
+package com.sa.app.HeartiHealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartiHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartiHealthApplication.class, args);
+	}
+
+}
---
+++ HeartiHealth/src/test/java/com/sa/app/HeartiHealth/HeartiHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sa.app.HeartiHealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartiHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
