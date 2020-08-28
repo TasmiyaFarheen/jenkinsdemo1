3f7a0f223f73e58f19e575061252de4ab8d7c998
TFarheen <TFarheen@OPT-LAP-0375.Opteamix.COM>
2020-08-28 14:58:25 +0530

    2nd commit

--- jenkins/UserApp-2/src/main/java/com/opt/app/userapp/UserApp2Application.java
+++ jenkins/UserApp-2/src/main/java/com/opt/app/userapp/UserApp2Application.java
@@ -8,6 +8,7 @@ public class UserApp2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserApp2Application.class, args);
+		System.out.println("--------------");
 	}
 
 }
